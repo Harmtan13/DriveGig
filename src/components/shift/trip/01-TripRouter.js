6057a678197994d6f7785a8501f3986ad9e1614a
Tanner Harman <harmtan2@gmail.com>
2020-06-17 13:55:14 -0600

    Changed Active & total trips to rely on Trips state instead of localStorage

--- src/components/shift/trip/01-TripRouter.js
+++ src/components/shift/trip/01-TripRouter.js
@@ -6,23 +6,25 @@ import Pickup from './03-Pickup';
 import Departure from './04-Departure';
 import Trips from './05-Trips';
 import EndTrip from './06-EndTrip';
-import { setLocalStorage } from '../../../helpers/trips/TripHelpers';
+import { setLocalStorage, tripCounter } from '../../../helpers/trips/TripHelpers';
 import { useTrip, useTrips, } from '../../../helpers/trips/customHooks';
 
 export default function TripRouter() {
   const [trips, setTrips] = useTrips();
-  const [trip, setTrip] = useTrip();
-  const activeTrips = trips.filter(trip => trip.completed === false).length;
+  const [trip, setTrip] = useTrip(trips);
+
+  const tripsCounter = tripCounter(trips);
 
   const tripState = {
     trip,
     setTrip,
-    activeTrips
+    tripsCounter
   }
 
   useEffect(() => {
     setTrips(trip);
     setLocalStorage({trip, trips})
+    console.log(trips);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [trip]);
