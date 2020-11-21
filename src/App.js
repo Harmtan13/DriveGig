a1e0566c50ae2a78099507510b69a68e6e8f6eea
Tanner Harman <harmtan13@gmail.com>
2020-11-21 10:54:38 -0700

    Added Erase Everything button for testing purposes.

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ export default function App() {
     setShift,
   };
 
+  const eraseLocalStorage = () => {
+    localStorage.clear();
+  };
+
   useEffect(() => {
     if (shift.completed) {
       localStorage.clear();
@@ -37,6 +41,12 @@ export default function App() {
           <Route exact path = "/">
             <ButtonWrapper className = "wrapper">
               <h1>Welcome to Drive-Gig</h1>
+
+              <div>
+                <button onClick = {eraseLocalStorage} className = "erase">
+                  Erase Everything
+                </button>
+              </div>
 
               <Link to = "/start-shift">
                 <Button>
