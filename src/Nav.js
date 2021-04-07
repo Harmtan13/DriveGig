ebb557cab605c0107fb3f13f085692f14afb4d3c
Tanner Harman <harmtan13@gmail.com>
2021-04-07 10:16:50 -0600

    Added firebase Auth into application

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
     <header>
-      <Link to='/'>
+      <Link to = "/">
         <h1>DriveGig</h1>
       </Link>
     </header>
-  )
+  );
 }
