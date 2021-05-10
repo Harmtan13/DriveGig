ae434c65f01e1caf1a66693e09c25bac6b846f89
Tanner Harman <harmtan13@gmail.com>
2021-05-09 20:37:25 -0600

    Changed link

--- src/components/shift/03-PauseShift.js
+++ src/components/shift/03-PauseShift.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import { addTimeStamp } from '../../helpers/ShiftHelpers';
 
 export default function PauseShift({ shift, setShift, setStage }) {
-  setStage('/shift');
+  setStage('/active-shift');
   const resumeShift = () => {
     addTimeStamp(setShift, shift, 1);
   };
 
   return (
     <div>
-      <Link to = "/shift">
+      <Link to = "/active-shift">
         <button
           onClick = {resumeShift}
         >
